8ea3c8039889354c61e105e4e5921f35956aebb2
CesarCedanoV <CesarCedanoV@gmail.com>
2019-05-14 01:34:09 -0400

    Started two other endpoints that we will need them in our API later

--- main.go
+++ main.go
@@ -13,13 +13,37 @@ func main() {
 	// Create a group of router with the path prefix '/api/'
 	apiRoutes := router.Group("/api")
 	{
-		apiRoutes.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Hello World",
-			})
-		})
+		apiRoutes.GET("/", HealthCheck)
+
+		apiRoutes.GET("/jokes", GetJokes)
+		apiRoutes.POST("/jokes/like/:jokeID", LikeJoke)
+
 	}
 
 	// Start and Run the Server on port 9000
 	router.Run(":9000")
+
+}
+
+// HealthCheck ping the server and expect a StatusOK
+func HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Healt Check",
+	})
+}
+
+// GetJokes return a list of Jokes
+func GetJokes(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "This endpoint will show a list of jokes",
+	})
+}
+
+// LikeJoke return the total of likes of a specific Joke
+func LikeJoke(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "This endpoint will give a like to a joke.",
+	})
 }
