a744b3c22332de81a0e5d07638370d53be2fee33
CesarCedanoV <CesarCedanoV@gmail.com>
2019-05-16 13:26:40 -0400

    Created the Joke struct and a array of Jokes. Also return that jokes array when call the GetJokes function

--- main.go
+++ main.go
@@ -7,6 +7,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Joke will contain information about a joke
+type Joke struct {
+	ID      int    `json:"id" binding:"required"`
+	Likes   int    `json:"likes"`
+	Content string `json:"content" binding:"required"`
+}
+
+var jokes = []Joke{
+	Joke{1, 0, "JOKE 1"},
+	Joke{2, 0, "JOKE 2"},
+	Joke{3, 0, "JOKE 3"},
+	Joke{4, 0, "JOKE 4"},
+	Joke{5, 0, "JOKE 5"},
+	Joke{6, 0, "JOKE 6"},
+	Joke{7, 0, "JOKE 7"},
+	Joke{8, 0, "JOKE 8"},
+}
+
 func main() {
 	// Create a router from the Gin's Default router
 	router := gin.Default()
@@ -39,9 +57,7 @@ func HealthCheck(c *gin.Context) {
 // GetJokes return a list of Jokes
 func GetJokes(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "This endpoint will show a list of jokes",
-	})
+	c.JSON(http.StatusOK, jokes)
 }
 
 // LikeJoke return the total of likes of a specific Joke
